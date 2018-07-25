34403f16f08480e13920025d57d95e5de054d59c
luckzman <lumpsey@gmail.com>
2018-07-25 23:42:16 +0100

    setup webpack, eslint and babel

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './client/app/index.js',
+  output: {
+    path: path.resolve('dist/app'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
